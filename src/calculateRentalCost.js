da51dafad32834730248dfa5b726739e27f0c24e
Marianna Matola <mutnenka.mk@gmail.com>
2025-02-12 02:12:30 +0200

    save file

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const costPerDay = 40;
+  const totalCost = costPerDay * days;
+  const longTerm = 7;
+  const LongTermDiscount = 50;
+  const middleTerm = 3;
+  const middleTermDiscount = 20;
+
+  if (days >= longTerm) {
+    return totalCost - LongTermDiscount;
+  }
+
+  if ((days >= middleTerm) & (days < longTerm)) {
+    return totalCost - middleTermDiscount;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
